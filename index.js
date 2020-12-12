8b91ad094e0044cc0a712747400f6b8cfdbebf20
Hongjun-Kim <dromide@naver.com>
2020-12-12 10:49:35 +0900

    parcel -10

---
+++ index.js
@@ -0,0 +1,8 @@
+import test from "./js/test.js";
+import "./scss/index.scss";
+
+const run = () => {
+  console.log("Yo i am runnin");
+};
+
+test();
---
+++ js/test.js
@@ -0,0 +1,5 @@
+const test = () => {
+  console.log("this is from the Test File");
+};
+
+export default test;
